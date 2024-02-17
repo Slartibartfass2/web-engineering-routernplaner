bae037b9dda65f8d52ca4d6ef7c5e84c67b75bba
Felix Schlegel <felix.schlegel@gmx.de>
2024-02-17 02:07:29 +0100

    feat: Stops are fetched and inserted as suggestions and list of stops

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,42 @@ if (mapImg !== null) {
   (mapImg as HTMLImageElement).src = MapImg;
 }
 
-getStopList()
-  .then((stops) => {
-    const stations = stops.map((stop) => `<option value="${stop}"></option>`).join();
-    const datalist = document.getElementById("stations")
-    if (datalist !== null) {
-      datalist.innerHTML = stations;
-    }
-  })
+let stations: string[] = [];
+function setStopList(stops: string[]) {
+  const stopList = document.getElementById("stop-list");
+  if (stopList !== null) {
+    stopList.innerHTML = stops
+      .map(
+        (stop) =>
+          `<li><div>${stop}</div><input type="checkbox" name="${stop}" /></li>`
+      )
+      .join("");
+  }
+}
+
+function setOptions(stops: string[]) {
+  const options = stops
+    .map((stop) => `<option value="${stop}"></option>`)
+    .join("");
+  const datalist = document.getElementById("stations");
+  if (datalist !== null) {
+    datalist.innerHTML = options;
+  }
+}
+
+getStopList().then((stops) => {
+  stations = stops;
+  setOptions(stops);
+  setStopList(stops);
+});
+
+const filter = document.getElementById(
+  "stop-filter"
+) as HTMLInputElement | null;
+filter?.addEventListener("input", function () {
+  const searchText = this.value.toLowerCase();
+  const suggestions = stations.filter((station) =>
+    station.toLowerCase().includes(searchText)
+  );
+  setStopList(suggestions);
+});
